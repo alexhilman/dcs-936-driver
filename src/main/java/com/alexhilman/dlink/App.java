f1143aa03bbec113c7a948d6faadd85184d8a82f
Alex Hilman <ahilman@northstarfleet.com>
2017-07-21 23:20:06 -0600

    Adding assembly description

--- src/main/java/com/alexhilman/dlink/App.java
+++ src/main/java/com/alexhilman/dlink/App.java
@@ -2,12 +2,9 @@ package com.alexhilman.dlink;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
