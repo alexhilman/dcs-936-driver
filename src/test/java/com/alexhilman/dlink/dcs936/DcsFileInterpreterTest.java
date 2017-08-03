fc78d1747da1aafd419e6bff6ad03ec8924101f8
Alex Hilman <ahilman@northstarfleet.com>
2017-08-02 21:01:46 -0600

    Ensuring parsing of secondary structure

--- src/test/java/com/alexhilman/dlink/dcs936/DcsFileInterpreterTest.java
+++ src/test/java/com/alexhilman/dlink/dcs936/DcsFileInterpreterTest.java
@@ -12,6 +12,7 @@ import static org.hamcrest.Matchers.*;
 
 public class DcsFileInterpreterTest {
     private static final String rootStructureResource = "/com/alexhilman/dlink/dcs936/samples/root-structure.xml";
+    private static final String secondaryStructureResource = "/com/alexhilman/dlink/dcs936/samples/secondary-structure.xml";
 
     private final DcsFileInterpreter dcsFileInterpreter = new DcsFileInterpreter();
 
@@ -43,6 +44,38 @@ public class DcsFileInterpreterTest {
             assertThat(fourthFile.isDirectory(), is(true));
             assertThat(fifthFile.isDirectory(), is(true));
         } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    @Test
+    public void shouldReadSecondaryFolderStructure() {
+        try (final InputStream rootStructureStream = getClass().getResourceAsStream(secondaryStructureResource)) {
+            assertThat(rootStructureStream, is(notNullValue()));
+
+            final List<DcsFile> dcsFiles = dcsFileInterpreter.interpret(rootStructureStream);
+
+            assertThat(dcsFiles, is(notNullValue()));
+            assertThat(dcsFiles, hasSize(5));
+
+            final DcsFile firstFile = dcsFiles.get(0);
+            final DcsFile secondFile = dcsFiles.get(1);
+            final DcsFile thirdFile = dcsFiles.get(2);
+            final DcsFile fourthFile = dcsFiles.get(3);
+            final DcsFile fifthFile = dcsFiles.get(4);
+
+            assertThat(firstFile.getFileName(), is("21"));
+            assertThat(secondFile.getFileName(), is("20"));
+            assertThat(thirdFile.getFileName(), is("19"));
+            assertThat(fourthFile.getFileName(), is("18"));
+            assertThat(fifthFile.getFileName(), is("17"));
+
+            assertThat(firstFile.isDirectory(), is(true));
+            assertThat(secondFile.isDirectory(), is(true));
+            assertThat(thirdFile.isDirectory(), is(true));
+            assertThat(fourthFile.isDirectory(), is(true));
+            assertThat(fifthFile.isDirectory(), is(true));
+        } catch (Exception e) {
             throw new AssertionError(e);
         }
     }
